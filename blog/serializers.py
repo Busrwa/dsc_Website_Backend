8d16ed81e06772e1be0c04306fd843c4e5ac2de4
BusrwaBusrwa <1999deneme2003@gmail.com>
2025-11-14 17:08:18 +0300

    Updated blog serilizers

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,14 +1,14 @@
-# blog/serializers.py
 from rest_framework import serializers
 from .models import Blog
 
-
 class BlogSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Blog
-        fields = '__all__'
+        fields = "__all__"
 
     def get_image(self, obj):
         if obj.image:
-            return obj.image.url  # Cloudinary tam URL
+            return obj.image.url   # Cloudinary tam URL
         return None
