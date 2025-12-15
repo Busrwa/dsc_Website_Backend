94444de96ad5c92a3c780383a6faa8051f8e0f88
Busra Yagcioglu <busrayagcioglu2003@gmail.com>
2025-12-15 16:06:59 +0300

    Updated team timezone

--- team/models.py
+++ team/models.py
@@ -1,6 +1,7 @@
 # team/models.py
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.utils.timezone import now
 
 
 class TeamMember(models.Model):
@@ -14,7 +15,7 @@ class TeamMember(models.Model):
         help_text="Takım üyesi fotoğrafı"
     )
     bio = models.TextField(blank=True, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=now)
 
     def __str__(self):
         return self.name
